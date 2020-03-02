2cd92cf98a55448061fdc2be76abb0348d92f71a
pioneerLee <kde740998@gmail.com>
2020-03-02 23:49:42 +0900

    normaluser update

--- routes/mypage.js
+++ routes/mypage.js
@@ -14,10 +14,62 @@ const fs = require('fs');
 
 
 //보유목록
+router.get('/emojipacks',auth.authenticate(),async(req,res,next)=>{
+    try{
+        const exNormaluser = await Normaluser.findOne({user:req.user._id});
+        if(exNormaluser){
+            res.status(200).json(exNormaluser.emojipacks);
+        }else{
+            res.sendStatus(204);
+        }
+    }catch(error){
+        next(error);
+    }
+});
 
 //구매목록
 
-//
+//추가 연동한 이메일 목록
+router.get('/emails',auth.authenticate(),async(req,res,next)=>{
+    try{
+        const exNormaluser = await Normaluser.findOne({user:req.user._id});
+        if(exNormaluser){
+            res.status(200).json(exNormaluser.linked_email);
+        }else{
+            res.sendStatus(204);
+        }
+    }catch(error){
+        next(error);
+    }
+});
+
+//연결된 서비스 목록
+router.get('/services',auth.authenticate(),async(req,res,next)=>{
+    try{
+        const exNormaluser = await Normaluser.findOne({user:req.user._id});
+        if(exNormaluser){
+            res.status(200).json(exNormaluser.services);
+        }else{
+            res.sendStatus(204);
+        }
+    }catch(error){
+        next(error);
+    }
+});
+
+//이메일 추가 연동하기
+router.patch('/newemail',auth.authenticate(),async(req,res,next)=>{
+    try{
+        
+    }catch(error){
+        next(error);
+    }
+});
+//이메일 인증하기
+
+//추가 연동한 이메일 삭제하기
+
+
 
 
 module.exports = router;
